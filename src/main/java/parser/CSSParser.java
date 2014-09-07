834d8d943fb2e1b491ac28fa8219877b0de48043
Ryan Magani <ryanmagani@gmail.com>
2014-09-07 13:28:19 -0700

    Added basic skeleton files for parser

---
+++ src/main/java/parser/CSSParser.java
@@ -0,0 +1,20 @@
+package parser;
+
+import java.util.List;
+import java.util.Map;
+
+public class CSSParser {
+
+	/**
+	 * Parses a map of CSS selectors to declarations from the given file
+	 * 
+	 * @param filename
+	 *            The name of the file to parse
+	 * @requires the file is a properly formatted CSS file
+	 * @return a map whose keys are CSS selectors and values are lists of
+	 *         declarations
+	 */
+	public static Map<String, List<String>> parse(String filename) {
+		return null;
+	}
+}
