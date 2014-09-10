fa795aca3d0b8469e2693ff6931c2275c3273ac2
Ryan Magani <ryanmagani@gmail.com>
2014-09-10 16:39:42 -0700

    Changing project to be a Perl script rather than parser util and remover

---
+++ unused_css_remover.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+#
+use strict;
+use warnings;
+#perl -pi -e 's/\n//g' bootstrap.css					remove linebreaks
+#perl -pi -e 's/\/\*.*?\*\///g' bootstrap.css		remove comments
+#perl -pi -e 's/ +/ /g' bootstrap.css				everything into single spaces
+#perl -pi -e 's/<THING> *?{.*?}//g' bootstrap.css	remove thing
