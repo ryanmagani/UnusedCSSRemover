e931b3cad0d5e49b1f44352f762b6f59fa7556c7
Ryan Magani <ryanmagani@gmail.com>
2014-09-10 16:46:44 -0700

    Added notes for structure of script

--- unused_css_remover.pl
+++ unused_css_remover.pl
@@ -6,3 +6,17 @@ use warnings;
 #perl -pi -e 's/\/\*.*?\*\///g' bootstrap.css		remove comments
 #perl -pi -e 's/ +/ /g' bootstrap.css				everything into single spaces
 #perl -pi -e 's/<THING> *?{.*?}//g' bootstrap.css	remove thing
+
+#basic structure:
+# take in css file and file that is a list of selectors to remove
+# remove linebreaks/comments/extraneous spaces from css
+# remove comments/extraneous spaces from txt
+# for each line in txt
+#	remove that line followed by any white space, then { then any chars, then }
+
+# issues this will cause:
+# 	comments will be lost 
+#	linebreaks will be lost
+#	random spacing will be lost (but not in selectors assuming it's valid CSS)
+#	won't handle invalid css at all
+#	hard to auto-test
