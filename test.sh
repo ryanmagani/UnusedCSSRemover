4b69fdfbc5fc56d302242349177bd62c73c30f21
Ryan Magani <ryanmagani@gmail.com>
2014-09-12 16:44:55 -0700

    Added test for the entirety of Twitter Bootstrap

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Simple script to run the perl script with test files and diff the results against expected results
-declare -a cssFiles=("comment-test.css" "empty-selector-test.css" "general-test.css" "newline-test.css" "special-cases-test.css")
-declare -a txtFiles=("empty.txt" "empty.txt" "general-test.txt" "empty.txt" "special-cases-test.txt")
-declare -a cssFilesExpected=("comment-test-expected.css" "empty-selector-test-expected.css" "general-test-expected.css" "newline-test-expected.css" "special-cases-test-expected.css")
-declare -a cssFilesActual=("comment-test-actual.css" "empty-selector-test-actual.css" "general-test-actual.css" "newline-test-actual.css" "special-cases-test-actual.css")
+# If no output is produced, all tests passed
+declare -a cssFiles=("comment-test.css" "empty-selector-test.css" "general-test.css" "newline-test.css" "special-cases-test.css" "bootstrap.css")
+declare -a txtFiles=("empty.txt" "empty.txt" "general-test.txt" "empty.txt" "special-cases-test.txt" "bootstrap.txt")
+declare -a cssFilesExpected=("comment-test-expected.css" "empty-selector-test-expected.css" "general-test-expected.css" "newline-test-expected.css" "special-cases-test-expected.css" "bootstrap-expected.css")
+declare -a cssFilesActual=("comment-test-actual.css" "empty-selector-test-actual.css" "general-test-actual.css" "newline-test-actual.css" "special-cases-test-actual.css" "bootstrap-actual.css")
 declare -r path="test/"
 declare -i START=0
 declare -i END="${#cssFiles[@]}"
