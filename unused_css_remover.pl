73c36cf44f54e1f5dd4695424690c6c1f008e103
Ryan Magani <ryanmagani@gmail.com>
2014-09-13 09:35:02 -0700

    Changed tabs to spaces in attempt to format better on github

--- unused_css_remover.pl
+++ unused_css_remover.pl
@@ -1,19 +1,19 @@
 #!/usr/bin/perl
 # A script to remove empty and unused selectors from a CSS file
 # As a side effect of these operations, all comments and linebreaks are removed from the
-	# file, it is therefore recommended to run this script when minifying/compressing the file
+    # file, it is therefore recommended to run this script when minifying/compressing the file
 # This script is not a comprehensive minifier or compressor
 # This script makes no guarantees for syntactically incorrect CSS files
 # Attempting to remove a selector with nested elements (essentially the @media tag) will not
-	# work properly, however, if all of the child selectors are removed from the parent, then the
-	# parent will be removed
+    # work properly, however, if all of the child selectors are removed from the parent, then the
+    # parent will be removed
 # Single-line CSS-style comments are allowed in the text file, but multi-line are not
 
 # Usage:
 # ./unused_css_remover.pl [Original CSS File] [Text file] [New CSS File]
-#		Original CSS File: the CSS file from which selectors will be removed, this file will NOT be modified
-#		A \n separated list of selectors to remove from the file
-#		New CSS File: the file path/name of the modified file to create
+#       Original CSS File: the CSS file from which selectors will be removed, this file will NOT be modified
+#       A \n separated list of selectors to remove from the file
+#       New CSS File: the file path/name of the modified file to create
 
 use strict;
 use warnings;
@@ -23,7 +23,7 @@ use File::Slurp;
 my $numberOfArguments = $#ARGV + 1;
 if ($numberOfArguments != 3)
 {
-	die "Usage: unused_css_remover.pl [CSS_FILE_TO_REMOVE_FROM] [TXT_SELECTORS_TO_REMOVE] [CSS_FILE_TO_CREATE]";
+    die "Usage: unused_css_remover.pl [CSS_FILE_TO_REMOVE_FROM] [TXT_SELECTORS_TO_REMOVE] [CSS_FILE_TO_CREATE]";
 }
 
 # Read the arguments
@@ -51,8 +51,8 @@ $cssContent =~ s/\/\*.*?\*\///g;
 # Remove all the selectors from the text file
 foreach (@selectors)
 {
-	# see end of file
-	$cssContent =~ s/((?<=\})|(?<=\{)|^)\s*\Q$_\E\s*\{.*?\}//g;
+    # see end of file
+    $cssContent =~ s/((?<=\})|(?<=\{)|^)\s*\Q$_\E\s*\{.*?\}//g;
 }
 
 # Remove all empty selectors (any selector with only whitespace between it's curly braces)
@@ -65,25 +65,30 @@ $cssContent =~ s/[^\{\}]*\{\s*\}//g;
 write_file("$newCSS", "$cssContent");
 
 # Regex explanations
-# s/\n//g												remove linebreaks
-#	\n 		 		new line
 
-# s/\/\*.*?\*\///g										remove comments
-#	\/\*			/*
-#	.*?				any char until the first
-#	\*\/			*/
+# s/\n//g                                               remove linebreaks
+#
+#   \n              new line
 
-# s/((?<=\})|(?<=\{)|^)\s*\Q$_\E\s*\{.*?\}//g				remove selector
-#	((?<=\})|(?<=\{)|^) 	before the match exists either }, { or the start of the string
-#	\s*				any whitespace
-#	\Q$_\E			the selector to find ($_) wrapped in \Q\E literal string quotes
-#	\s*				any whitespace until
-#	\}				{
-#	.*?				any char until the first
-#	\}				}
+# s/\/\*.*?\*\///g                                      remove comments
+#
+#   \/\*            /*
+#   .*?             any char until the first
+#   \*\/            */
 
-# s/[^\{\}]*\{\s*\}//g									remove any empty selectors, run twice
-#	[^\{\}]*		any char that isn't { or } until
-#	\{				{
-#	\s*				any whitespace until
-#	\}				}
+# s/((?<=\})|(?<=\{)|^)\s*\Q$_\E\s*\{.*?\}//g           remove selector
+#
+#   ((?<=\})|(?<=\{)|^)     before the match exists either }, { or the start of the string
+#   \s*                     any whitespace
+#   \Q$_\E                  the selector to find ($_ is the foreach value) wrapped in \Q\E literal string quotes
+#   \s*                     any whitespace until
+#   \}                      {
+#   .*?                     any char until the first
+#   \}                      }
+
+# s/[^\{\}]*\{\s*\}//g                                  remove any empty selectors, run twice
+#
+#   [^\{\}]*        any char that isn't { or } until
+#   \{              {
+#   \s*             any whitespace until
+#   \}              }
